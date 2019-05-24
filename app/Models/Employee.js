378827e547201c28129bbea7e10a82df8a3610b8
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-05-24 17:15:55 -0400

    employee schema created, instance methods created for employee model

---
+++ app/Models/Employee.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const md5 = require("md5");
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		index: true,
+		lowercase: true,
+		required: [true, "Email is required!"]
+	},
+	phone: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 10,
+		maxlength: 15
+	},
+	password:{
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	active: {type: Boolean, default: false},
+	activationToken: {type: String, default: ""},
+	activationTokenExpires: {type: Date, default: ""},
+	passwordResetToken: {type: String, default:""},
+	passwordResetExpires: {type: Date, default: ""},
+	role: {type: String, default: "staff", lowercase: true},
+	avatar: {type: String, default: "http://lorempixel.com/400/200/people"},
+}, {timestamps: true});
+
+EmployeeSchema.virtual("fullname").get(function(){
+	return this.firstName + " " + this.lastName;
+});
+
+EmployeeSchema.virtual('gravatar').get(function(){
+	const hash = md5(this.email);
+	return `https://gravatar.com/avatar/${hash}?s=200`;
+});
+
+EmployeeSchema.methods.isAdmin = function(){
+	return this.role === "admin";
+};
+
+EmployeeSchema.methods.detailsToJSON = function(){
+	const employeeInfo = {
+		_id: this._id,
+		email: this.email,
+		firstName: this.firstName,
+		lastName: this.lastName,
+		phone: this.phone
+	};
+
+	return employeeInfo;
+};
+
+const employee = mongoose.model("Employee", EmployeeSchema);
+
+module.exports = employee;
