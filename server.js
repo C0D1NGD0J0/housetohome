0d6ff50f632a6993380cf5b5aa133db5ca17dccf
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-07-16 16:07:18 -0400

    deployment setup

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const app = express();
+const path = require("path");
 const corsOptions = {
 	origin: '*',
 	optionsSuccessStatus: 200
